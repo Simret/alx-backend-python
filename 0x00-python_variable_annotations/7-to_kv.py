4dc8706c97aa511c5da5cbded6e70bf3e06648c5
simret <simruineb@gmail.com>
2023-02-02 23:20:52 +0300

    Tuple of mixed type

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Type annotated module'''
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v[Union[int, float]]) -> Tuple[str, float]:
+    '''Tuple of mixed type'''
+    return (k, float(v**2))
