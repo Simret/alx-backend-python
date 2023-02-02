ea9fd7df10ecd6868b7c1b03ea473ec0771a1e27
simret <simruineb@gmail.com>
2023-02-03 00:09:20 +0300

    DuckType Annotation

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Type annotated module'''
+
+from typing import Any, Sequence, Union
+
+# The types of the elements of the input are not know
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Duck type annotation'''
+    if lst:
+        return lst[0]
+    else:
+        return None
