c100320b36b5e359f0266a6006f24bb37d10a4d6
simret <simruineb@gmail.com>
2023-02-03 02:55:31 +0300

    Echo

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Type annotated module'''
+
+from typing import Any, Mapping, Union, TypeVar
+T = TypeVar('T')
+Res = Union[T, None]
+
+
+def safely_get_value(dct: Mapping, key: Any, default=None) -> Res:
+    ''''''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
