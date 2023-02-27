244d67e52adca2dcdfcab2e947a17919e8a033cd
simret <simruineb@gmail.com>
2023-02-27 03:14:16 +0300

    Parametize expand

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,10 @@ class TestAccessNestedMap(unittest.TestCase):
         response = access_nested_map(nested_map, path)
         self.assertEqual(response, expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
     def test_access_nested_map_exception(
             self,
             nested_map: Dict,
